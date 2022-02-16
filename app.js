bcea3ad801aeb9e3261104e08089aee8ceb6d18c
timothyrusso <85668106+timothyrusso@users.noreply.github.com>
2022-02-16 07:28:23 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const { generateMainView } = require('./views');
+const fs = require('fs');
+const path = require('path');
+
+const { PORT = 3000 } = process.env;
+
+const server = http.createServer((req, res) => {
+  
+  const dataPath = path.join(__dirname, 'data.json');
+  
+  fs.readFile(dataPath, { encoding: 'utf8' }, (err, data) => {
+      if (err) {
+    console.log(err);
+    return;
+  } else {
+    res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+  
+    const songs = JSON.parse(data);
+  const markup = generateMainView(songs);
+  res.end(markup);
+  }
+  });
+});
+
+server.listen(PORT);
